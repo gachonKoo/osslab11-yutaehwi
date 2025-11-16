68c0374c5e37e4a3e643f64c7368d373b91c4c37
yutaehwi <109072106+yutaehwi@users.noreply.github.com>
2025-11-16 18:49:51 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,21 +1,4 @@
 import sys
-from lab11.geo.area import square_area, triangle_area
-from lab11.geo.distance import distance
 
-def test_square():
-    assert square_area(4) == 16
-
-def test_triangle():
-    assert triangle_area(10, 5) == 25.0
-
-def test_distance():
-    assert distance(0, 0, 3, 4) == 5.0
-
-if __name__ == "__main__":
-    test_square()
-    test_triangle()
-    test_distance()
-
-    student_id = sys.stdin.read().strip()
-
-    print(f"Success {student_id}")
+student_id = sys.stdin.read().strip()
+print(f"Success {student_id}")
