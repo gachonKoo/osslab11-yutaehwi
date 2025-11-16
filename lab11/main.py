2a9397c44d504325d1337544695caed03a8b2659
yutaehwi <109072106+yutaehwi@users.noreply.github.com>
2025-11-16 18:26:37 +0900

    Delete lab11 directory

--- lab11/main.py
+++
@@ -1,6 +0,0 @@
-from geo.area import square_area, triangle_area
-from geo.distance import distance
-
-print("Square:", square_area(4))
-print("Triangle:", triangle_area(10, 5))
-print("Distance:", distance(0, 0, 3, 4))
